ca6365f86ca78a178ac064e35ceca24f2c129ca3
ezist <maxwell1998r@gmail.com>
2020-09-29 14:03:04 -0500

    Link to user's profile in header
    Signed-off-by: ezist <maxwell1998r@gmail.com>

--- static/header.php
+++ static/header.php
@@ -1,3 +1,16 @@
+<?php
+function getUserFromUsername($username, $connection) {
+	$stmt = $connection->prepare("SELECT * FROM `users` WHERE `username` = ?");
+	$stmt->bind_param("s", $username);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$user = $result->fetch_assoc();
+	if ($result->num_rows === 0) return('That user does not exist.');
+	$stmt->close();
+
+	return $user;
+}
+?>
 <div class="headerTop">
     <a href="/index.php"><img src="/static/spacemy.png"></a> &nbsp; <small id="floatRight"><a href="/login.php">Login</a> &bull; <a href="/register.php">Register</a>&nbsp;&nbsp;</small><br>
     <span id="floatRight">
@@ -19,15 +32,15 @@
         <a href="/friends/">Friends</a> &bull;
         <a href="/jukebox.php">Jukebox</a> &bull;
         <a href="/users.php">All Users</a>
-        <?php if(isset($_SESSION['siteusername'])) {?>
+        <?php if (isset($_SESSION['siteusername'])) {?>
         <span id="floatRight">
             <span id="custompadding">
-                <a href='/edit'>Edit Items</a> &bull;
-                <a href='/manage.php'>Manage User</a> &bull;
-                <?php echo htmlspecialchars($_SESSION['siteusername'])?>
+                <a href="/edit">Edit Items</a> &bull;
+                <a href="/manage.php">Manage User</a> &bull;
+				<a href="/profile.php?id=<?php echo(htmlspecialchars(getUserFromUsername($_SESSION['siteusername'], $conn)["id"]));?>"><?php echo($_SESSION['siteusername'])?></a>
             </span>
         </span>
-        ?>
+        <?php }?>
     </small>
 </div>
 <?php
@@ -38,5 +51,5 @@ if(isset($_SESSION['siteusername'])) {
     $result = $stmt->get_result();
     if($result->num_rows === 0) die("U have been ban");
     $stmt->close();
-}
+};
 ?>
