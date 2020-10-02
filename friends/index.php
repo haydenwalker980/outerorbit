64ad6cf08b68d7747711290fc0e3f25b1b2d2d34
ezist <maxwell1998r@gmail.com>
2020-10-02 12:22:56 -0500

    Add outgoing friend requests and revamp friends look
    Signed-off-by: ezist <maxwell1998r@gmail.com>

--- friends/index.php
+++ friends/index.php
@@ -27,21 +27,52 @@
             ?>
             <br>
             <div class="padding">
-                <div class="login">
-                    <div class="loginTopbar">
-                        <b>Incoming Friend Requests</b>
-                    </div>
-                    <div class="grid-container">
-                        <?php 
+                <div style="overflow:auto;">
+                    <div class="login" style="width:calc(50% - 10px);float:left;">
+                        <div class="loginTopbar">
+                            <b>Incoming Friend Requests</b>
+                        </div>
+                        <ul>
+                            <?php 
                                 $stmt = $conn->prepare("SELECT * FROM friends WHERE reciever = ? AND (status != 'a' AND status != 'd')");
                                 $stmt->bind_param("s", $_SESSION['siteusername']);
-                            $stmt->execute();
-                            $result = $stmt->get_result();
-                            if($result->num_rows === 0) echo('You have no incoming friend requests.');
-                            while($row = $result->fetch_assoc()) {
-                        ?>
-                            <div class="item1"><a href="/profile.php?id=<?php echo getIDFromUser($row['sender'], $conn); ?>"><div><b><center><?php echo $row['sender']; ?></center></b></div><img src="/dynamic/pfp/<?php echo getPFPFromUser($row['sender'], $conn); ?>"></a><br><a href="accept.php?id=<?php echo $row['id']; ?>"><button>Accept</button></a> <a href="deny.php?id=<?php echo $row['id']; ?>"><button>Deny</button></a></div>
-                        <?php } ?>
+                                $stmt->execute();
+                                $result = $stmt->get_result();
+                                if($result->num_rows === 0) echo('<li>You have no incoming friend requests.</li>');
+                                while($row = $result->fetch_assoc()) {
+                            ?>
+                            <li>
+                                <a href="/profile.php?id=<?php echo getIDFromUser($row['sender'], $conn); ?>">
+                                    <b><?php echo $row['sender']?></b>
+                                    <img style="height:1em;width:1em;" src="/dynamic/pfp/<?php echo getPFPFromUser($row['sender'], $conn)?>">
+                                </a><br>
+                                <a href="/friends/accept.php?id=<?php echo $row['id']?>"><button>Accept</button></a>
+                                <a href="/friends/deny.php?id=<?php echo $row['id']?>"><button>Deny</button></a>
+                            </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                    <div class="login" style="width:calc(50% - 10px);float:right;">
+                        <div class="loginTopbar">
+                            <b>Outgoing Friend Requests</b>
+                        </div>
+                        <ul>
+                            <?php 
+                                $stmt = $conn->prepare("SELECT * FROM friends WHERE sender = ? AND (status != 'a' AND status != 'd')");
+                                $stmt->bind_param("s", $_SESSION['siteusername']);
+                                $stmt->execute();
+                                $result = $stmt->get_result();
+                                if($result->num_rows === 0) echo('<li>You have no outgoing friend requests.</li>');
+                                while($row = $result->fetch_assoc()) {
+                            ?>
+                            <li>
+                                <a href="/profile.php?id=<?php echo getIDFromUser($row['sender'], $conn); ?>">
+                                    <b><?php echo $row['reciever']?></b>
+                                    <img style="height:1em;width:1em;" src="/dynamic/pfp/<?php echo getPFPFromUser($row['reciever'], $conn)?>">
+                                </a>
+                            </li>
+                            <?php } ?>
+                        </ul>
                     </div>
                 </div><br>
                 <div class="login">
