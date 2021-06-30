f9f99c5b9b9b098ea142f6652f80d171b636cd3e
Hayden <37120379+haydenwalker980@users.noreply.github.com>
2021-06-29 18:03:26 -0700

    i am in severe pain

--- static/changelog.php
+++ static/changelog.php
@@ -1,8 +1,1 @@
-<small>
-    <ul>
-        <li><b>jgdgdr344444</b> <small><u>fe39840 by typicalname0</u></small></li>
-        <li><b>Add dark mode (if this breaks I'm sorry)</b> <small><u>4c4a9f8 by ezist</u></small></li>
-        <li><b>Create CODE_OF_CONDUCT.md</b> <small><u>d8181fc by the-real-sumsome</u></small></li>
-        <li><b>Update issue templates</b> <small><u>740879b by the-real-sumsome</u></small></li>
-    </ul>
-</small>
+no, fuck you
